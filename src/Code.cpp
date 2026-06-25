789b4ec645a406525c4144835f4cf35d03229479
Nurtrantpoem19 <nurtrantpoem19@gmail.com>
2026-06-25 15:24:34 -0400

    compelte code implementation

---
+++ src/Code.cpp
@@ -0,0 +1,239 @@
+#include "translator/Code.hpp"
+#include <filesystem>
+
+int Code::labelCount = 0;
+int Code::staticAddress = 16;
+
+void Code::popToD()
+{
+    output << "@SP\n";
+    output << "AM=M-1\n";
+    output << "D=M\n";
+}
+void Code::popToM()
+{
+    output << "@SP\n";
+    output << "AM=M-1\n";
+}
+void Code::pushTrue()
+{
+    output << "@SP\n";
+    output << "A=M\n";
+    output << "M=-1\n";
+    output << "@SP\n";
+    output << "M=M+1\n";
+}
+void Code::pushFalse()
+{
+    output << "@SP\n";
+    output << "A=M\n";
+    output << "M=0\n";
+    output << "@SP\n";
+    output << "M=M+1\n";
+}
+
+void Code::writeBinOp(const std::string &op)
+{
+    popToD();
+    popToM();
+
+    output << "M=M" << op << "D\n";
+    output << "@SP\n";
+    output << "M=M+1\n";
+}
+
+void Code::writeUnOp(const std::string &op)
+{
+    popToM();
+    output << "M=" << op << "M\n";
+    output << "@SP\n";
+    output << "M=M+1\n";
+}
+void Code::writeCompLabel(const std::string &jump)
+{
+
+    ++labelCount;
+    popToD();
+    popToM();
+
+    output << "D=M-D\n";
+    output << "@TRUE" << labelCount << "\n";
+    output << "D;" << jump << std::endl;
+
+    pushFalse();
+    output << "@END_COMP" << labelCount << "\n";
+    output << "0;JMP\n";
+
+    output << "(TRUE" << labelCount << ")\n";
+    pushTrue();
+
+    output << "(END_COMP" << labelCount << ")\n";
+}
+
+Code::Code(const std::filesystem::path &outpath)
+    : output(outpath), currentFileName(outpath.stem().string()),
+      table{{"local", "LCL"}, {"argument", "ARG"}, {"this", "THIS"},
+            {"that", "THAT"}, {"pointer", ""},     {"constant", ""}},
+      opTable{{"add", OpType::Add}, {"sub", OpType::Sub}, {"neg", OpType::Neg},
+              {"eq", OpType::Eq},   {"gt", OpType::Gt},   {"lt", OpType::Lt},
+              {"and", OpType::And}, {"or", OpType::Or},   {"not", OpType::Not}},
+      segmentTable{
+          {"constant", SegmentType::Constant}, {"local", SegmentType::Local},
+          {"argument", SegmentType::Argument}, {"this", SegmentType::This},
+          {"that", SegmentType::That},         {"temp", SegmentType::Temp},
+          {"pointer", SegmentType::Pointer},   {"static", SegmentType::Static}}
+{
+}
+
+void Code::writeArithmetic(const std::string &command)
+{
+    OpType op = opTable.at(command);
+
+    switch (op)
+    {
+    // -------------------
+    // binary operations
+    // -------------------
+    case OpType::Add:
+        writeBinOp("+");
+        break;
+
+    case OpType::Sub:
+        writeBinOp("-");
+        break;
+
+    case OpType::And:
+        writeBinOp("&");
+        break;
+
+    case OpType::Or:
+        writeBinOp("|");
+        break;
+
+    // -------------------
+    // unary operations
+    // -------------------
+    case OpType::Neg:
+        writeUnOp("-");
+        break;
+
+    case OpType::Not:
+        writeUnOp("!");
+        break;
+
+    // -------------------
+    // comparisons
+    // -------------------
+    case OpType::Eq:
+        writeCompLabel("JEQ");
+        break;
+
+    case OpType::Gt:
+        writeCompLabel("JGT");
+        break;
+
+    case OpType::Lt:
+        writeCompLabel("JLT");
+        break;
+    }
+}
+
+void Code::writePushPop(Parser::CommandType command, std::string &segment,
+                        int index)
+{
+
+    std::string seg = table.at(segment);
+    SegmentType type = segmentTable.at(segment);
+    // going to do this in two phases, first is fetch cycle. getting
+    // the value from the target address. and then manipulate data accordingly
+    switch (type)
+    {
+    case SegmentType::Local:
+    case SegmentType::Argument:
+    case SegmentType::This:
+    case SegmentType::That:
+    {
+        output << "@" << seg << "\n";
+        output << "D=M\n";
+        output << "@" << index << "\n";
+        output << "D=D+A\n";
+        break;
+    }
+    case SegmentType::Constant:
+    {
+        output << "@" << index << "\n";
+        output << "D=A\n";
+
+        break;
+    }
+
+    case SegmentType::Pointer:
+    {
+        if (index == 1)
+            output << "@THAT\n";
+        else if (index == 0)
+        {
+            output << "@THIS\n";
+        }
+        output << "D=A\n";
+        break;
+    }
+
+    case SegmentType::Static:
+    {
+        output << "@" << currentFileName << "." << index << "\n";
+        output << "D=A\n";
+
+        break;
+    }
+
+    case SegmentType::Temp:
+    {
+        // index + 5 because temp lives in RAM[5 to 12]
+        output << "@" << index + 5 << "\n";
+        output << "D=A\n";
+
+        break;
+    }
+    }
+    output << "@15\nM=D\n";
+
+    if (command == Parser::CommandType::C_Push)
+    {
+        output << "@R15\n";
+        if (type != SegmentType::Constant)
+        {
+            output << "A=M\n";
+        }
+        output << "D=M\n";
+
+        output << "@SP\n";
+        output << "A=M\n";
+        output << "M=D\n";
+
+        output << "@SP\n";
+        output << "M=M+1\n";
+    }
+
+    if (command == Parser::CommandType::C_Pop)
+    {
+        // 1. Decrement SP and fetch the top value from the stack into D
+        output << "@SP\n";
+        output << "AM=M-1\n";
+        output << "D=M\n";
+
+        // 2. Write that value directly to the destination address waiting in
+        // R15
+        output << "@R15\n";
+        output << "A=M\n";
+        output << "M=D\n";
+    }
+}
+
+void Code::close()
+{
+    if (output.is_open())
+    {
+        output.close();
+    }
+}
